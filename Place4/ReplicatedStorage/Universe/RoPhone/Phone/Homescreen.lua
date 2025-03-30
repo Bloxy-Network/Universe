8096433c9e35e0d25841161e07d2427da71116b4
Fittergem <99085141+fittergem@users.noreply.github.com>
2025-03-30 17:47:48 -0400

    Update Homescreen.lua

--- Place4/ReplicatedStorage/Universe/RoPhone/Phone/Homescreen.lua
+++ Place4/ReplicatedStorage/Universe/RoPhone/Phone/Homescreen.lua
@@ -1,5 +1,7 @@
 -- @ScriptType: ModuleScript
 
+local RunService = game:GetService("RunService")
+
 local CONFIG = require(script.Parent.Parent:WaitForChild("CONFIG"))
 local Spring = require(script.Parent:WaitForChild("Spring"))
 
@@ -10,6 +12,7 @@ local Signal = require(dependencies:WaitForChild("GoodSignal"))
 local Page = require(script:WaitForChild("Page"))
 local Dock = require(script:WaitForChild("Dock"))
 local InfoBar = require(script:WaitForChild("InfoBar"))
+local Swipe = require(script:WaitForChild("Swipe"))
 
 local Homescreen = {}
 Homescreen.__index = Homescreen
@@ -38,9 +41,102 @@ function Homescreen.new()
 	
 	self.InfoBar = InfoBar.new()
 	self.InfoBar.Button.Parent = self.Frame
+	self.InfoBar.Button.Visible = false
+	self.InfoBar.PageDots.GroupTransparency = 1
+	self.InfoBar.Button.BackgroundTransparency = 1
 
-	self.Pages = {} :: {[number]: {["Page"]: typeof(Page.new()), ["Grid"]: typeof(Grid.new())}}
+	self.CurrentPage = 1
+	self.Pages = {} :: {[number]: typeof(Page.new())}
 	self.PageAdded = Signal.new()
+	
+	self.Swipe = Swipe.new(self.Frame)
+	
+	self.CurrentPageUpdated = Signal.new()
+	
+	self.Swipe.SwipeChanged:Connect(function(startPos: number, delta: number)
+		if #self.Pages > 1 then
+			local newPos = .5 + delta
+			
+			if self.CurrentPage == 1 then
+				newPos = math.clamp(newPos, -.5, .5)
+			end
+			
+			if self.CurrentPage == #self.Pages then
+				newPos = math.clamp(newPos, .5, 1.5)
+			end
+			
+			local newSpring = Spring.new(self.Pages[self.CurrentPage].Frame, 1.5, 8, {Position = UDim2.new(newPos, 0,.5,0)})
+			newSpring:Play()
+			
+			for i, page in self.Pages do
+				if i ~= self.CurrentPage then
+					local delta = i - self.CurrentPage
+					page.Frame.Position = UDim2.new(self.Pages[self.CurrentPage].Frame.Position.X.Scale + delta, 0,.5,0)
+				end
+			end
+		end
+	end)
+	
+	self.Swipe.Swiped:Connect(function(accel: number)
+		if #self.Pages <= 1 then
+			return
+		end
+				
+		local page = self.Pages[self.CurrentPage]
+				
+		if accel >= 10 then
+			if page.Frame.Position.X.Scale > .5 then
+				self.CurrentPageUpdated:Fire(self.CurrentPage - 1)
+			elseif page.Frame.Position.X.Scale < .5 then
+				self.CurrentPageUpdated:Fire(self.CurrentPage + 1)
+			end
+		elseif page.Frame.Position.X.Scale >= .9 then
+			self.CurrentPageUpdated:Fire(self.CurrentPage - 1)
+		elseif page.Frame.Position.X.Scale <= .1 then
+			self.CurrentPageUpdated:Fire(self.CurrentPage + 1)
+		else
+			local newSpring = Spring.new(page.Frame, 1.5, 5, {Position = UDim2.new(.5,0,.5,0)})
+			newSpring:Play()
+		end
+	end)
+	
+	self.CurrentPageUpdated:Connect(function(currentPage: number)
+		self.CurrentPage = currentPage
+				
+		local newSpring = Spring.new(self.Pages[self.CurrentPage].Frame, 1.5, 5, {Position = UDim2.new(.5,0,.5,0)})
+		newSpring:Play()
+		
+		self.Pages[self.CurrentPage].Frame.Changed:Connect(function(property)
+			if property ~= "Position" then
+				return
+			end
+			
+			for i, page in self.Pages do
+				if i ~= self.CurrentPage then
+					local delta = i - self.CurrentPage
+					page.Frame.Position = UDim2.new(self.Pages[self.CurrentPage].Frame.Position.X.Scale + delta,0,.5,0)
+				end
+			end
+		end)
+		
+		for i, dot in self.InfoBar.Dots do
+			if i == self.CurrentPage then
+				Spring.new(dot, 1.2, 3, {BackgroundTransparency = 0}):Play()
+			else
+				Spring.new(dot, 1.2, 3, {BackgroundTransparency = .5}):Play()
+			end
+		end
+	end)
+	
+	self.InfoBar.DotAdded:Connect(function(newDot: TextButton, index: number)
+		newDot.MouseButton1Click:Connect(function()
+			self.CurrentPageUpdated:Fire(index)
+		end)
+		
+		if index == self.CurrentPage then
+			Spring.new(newDot, 1.2, 3, {BackgroundTransparency = 0}):Play()
+		end
+	end)
 	
 	self.AppButtonAdded = Signal.new()
 	self.AppButtonDocked = Signal.new()
@@ -51,10 +147,18 @@ end
 function Homescreen:AddPage()
 	local page = Page.new()
 	page.Frame.Parent = self.Frame
+	page.Frame.Position = UDim2.new(.5+#self.Pages,0,.5,0)
 	
 	local index = #self.Pages + 1
 	
 	self.Pages[index] = page
+	
+	if #self.Pages > 1 then
+		self.InfoBar.Button.Visible = true
+		
+		Spring.new(self.InfoBar.Button, 1.5, 3, {BackgroundTransparency = 0}):Play()
+		Spring.new(self.InfoBar.PageDots, 1.5, 3, {GroupTransparency = 0}):Play()
+	end
 	
 	self.InfoBar:AddPageDot()
 	
